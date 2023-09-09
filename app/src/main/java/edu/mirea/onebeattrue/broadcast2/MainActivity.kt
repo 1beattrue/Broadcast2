7e82c8b84892a3af3f38c2e4d4a2fa4212e3aa8c
Vitaly <onebeattrue@gmail.com>
2023-09-09 15:42:24 +0300

    Приложение для тестирования

--- app/src/main/java/edu/mirea/onebeattrue/broadcast2/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/broadcast2/MainActivity.kt
@@ -1,11 +1,40 @@
 package edu.mirea.onebeattrue.broadcast2
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var progressBar: ProgressBar
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == "loaded") {
+                val percent = intent.getIntExtra("percent", 0)
+                progressBar.progress = percent
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progressBar = findViewById(R.id.progressBar)
+
+        val intentFilter = IntentFilter().apply {
+            addAction("loaded")
+        }
+        registerReceiver(receiver, intentFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
